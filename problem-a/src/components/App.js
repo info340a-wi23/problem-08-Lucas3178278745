ec7dffdc033cc9a62c833ffb68c723b0993e6bac
Lucas3178278745 <muyangzhou8@gmail.com>
2023-03-05 00:39:39 -0800

    Finish a

--- problem-a/src/components/App.js
+++ problem-a/src/components/App.js
@@ -5,6 +5,7 @@ import PetList from './PetList';
 import AboutPage from './AboutPage';
 import ResourcesPage from './ResourcesPage';
 import PetDetail from './PetDetail';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 
 import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
 
@@ -16,19 +17,21 @@ function App(props) {
     <div>
       <header className="jumbotron py-4 mb-4">
         <div className="container">
-          <h1>Adopt a Pet</h1>
+          <h1><Link to="/adopt">Adopt a Pet</Link></h1>
         </div>
       </header>
     
       <main className="container">
-        <div className="row">
-          <div className="col-3">
-            <AboutNav />
-          </div>
-          <div className="col">
-            <PetList pets={pets} />
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<PetList pets={pets} />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+          <Route path="/adopt" element={<PetPage />}>
+            <Route path=":petName" element={<PetDetail />} />
+            <Route index element={<PetList pets={pets} />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/adopt" />} />
+        </Routes>
       </main>
 
       <footer className="container">
